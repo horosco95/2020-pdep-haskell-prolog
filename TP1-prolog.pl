ff3aaed8cc66a9bb4a89ba4937ba730ad6157644
Hermes Orosco <hermesorosco2010@gmail.com>
2020-07-28 16:33:16 -0300

    fix punto 3

--- TP1-prolog.pl
+++ TP1-prolog.pl
@@ -14,17 +14,14 @@ perteneceA(monteDelDestino,mordor).
 camino([comarca,rivendel,moria,lothlorien,edoras,minasTirith,minasMorgul,monteDelDestino]).
 
 %% Punto 3
-zonasLimitrofes(rivendel,moria).
-zonasLimitrofes(moria,rivendel).
-zonasLimitrofes(moria,isengard).
-zonasLimitrofes(isengard,moria).
-zonasLimitrofes(lothlorien,edoras).
-zonasLimitrofes(edoras,lothlorien).
-zonasLimitrofes(edoras,minasTirith).
-zonasLimitrofes(minasTirith,edoras).
-zonasLimitrofes(minasTirith,minasMorgul).
-zonasLimitrofes(minasMorgul,minasTirith).
-zonasLimitrofes(UnaZona,OtraZona):- perteneceA(Region,UnaZona),perteneceA(Region,OtraZona),UnaZona\=OtraZona.
+limitrofes(rivendel,moria).
+limitrofes(moria,isengard).
+limitrofes(lothlorien,edoras).
+limitrofes(edoras,minasTirith).
+limitrofes(minasTirith,minasMorgul).
+zonasLimitrofes(UnaZona,OtraZona):-limitrofes(UnaZona,OtraZona).
+zonasLimitrofes(UnaZona,OtraZona):-limitrofes(OtraZona,UnaZona).
+zonasLimitrofes(UnaZona,OtraZona):- perteneceA(UnaZona,Region),perteneceA(OtraZona,Region),UnaZona\=OtraZona.
 
 %% Punto 4
 % parte A - 
