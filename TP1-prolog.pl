2607076a7570d26686512ed56503d14123f315fe
GZcreator <gzcreator92@gmail.com>
2020-08-02 21:50:48 -0300

    punto 1a tp 2

--- TP1-prolog.pl
+++ TP1-prolog.pl
@@ -62,9 +62,10 @@ primeras3ZonasMismaRegion([A,B,C|_]):- zonasLimitrofes(A,B),zonasLimitrofes(B,C)
 %% Punto 1
 % parte A - 
 
-accionUltimo(Zona, Cantidad):-perteneceA(Zona, Region),append(ListaRegiones, Region), not(member(Region, listaRegiones)
-cantidadRegiones(Zona, Cantidad):-accionUltimo(Zona,Cantidad). 
-cantidadRegiones([_|Zona], Cantidad):-cantidadRegiones(_, Cantidad), accionUltimo(Zona,Cantidad), length(ListaRegiones, Cantidad).
+cantidadDeRegiones(Camino,Cantidad):-
+    findall(Region,(member(Zona,Camino), perteneceA(Zona,Region)),ListaRegiones),
+    list_to_set(ListaRegiones,Lista),
+    length(Lista,Cantidad).
 
 % parte B - 
 esVueltero(Camino):- list_to_set(Camino,Camino).
